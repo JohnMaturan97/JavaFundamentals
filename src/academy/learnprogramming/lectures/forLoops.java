051a1abfce1ee3245763af7dfe2d39733806e689
JohnMaturan97 <JohnMaturan97@gmail.com>
2021-04-18 23:25:03 -0400

    JavaFundamentals

---
+++ src/academy/learnprogramming/lectures/forLoops.java
@@ -0,0 +1,64 @@
+package academy.learnprogramming.lectures;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import org.w3c.dom.ls.LSOutput;
+
+public class forLoops {
+
+    public static void main(String[] args) {
+
+for(int i = 0; i < 6; i++) {
+  //  System.out.println(StringArray[i]);
+}
+
+    }
+}
+
+//
+//        int[] intValArray = {5, 6};
+//        int[] intValArrray2 = {7, 8};
+//
+//        for (int i : intValArray) {
+//            System.out.print(i);
+//        }
+//        System.out.println(" ");
+//        for (int i : intValArrray2) {
+//            System.out.print(i);
+//
+//        }
+//        System.out.println("");
+//        int[] intValArray3;
+//        intValArray3 = new int[10];
+//        intValArray3[5] = 12;
+//        System.out.println(intValArray3[5]);
+//
+//    }
+//
+//}
+
+//        int[] intArray = {2, 5, 46, 12, 34};
+//
+//        for (int i : intArray) {
+//            System.out.print(i);
+//        }
+//
+//        System.out.println("");
+//        int[] intVariableArray = {2,5,46,12,34};
+//
+//        for(int i=0; i<intVariableArray.length; i++){
+//            System.out.print(intVariableArray[i]);
+//            // output: 25461234
+//        }
+//
+//        System.out.println(" ");
+//        int[] intValArray;
+//        intValArray = new int[10];
+//
+//        //array [5] = 12
+//        intValArray[5] = 12;
+//
+//        System.out.println(intValArray[5]);
+//        System.out.println(intValArray[1]);
+//    }
+//
+//}
